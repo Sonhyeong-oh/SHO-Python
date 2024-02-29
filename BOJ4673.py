169addcd602eda0a554f0972d34a6439c6eced6e
Sonhyeong-oh <daily6003@gmail.com>
2024-02-29 20:08:37 +0900

    feat: 셀프 넘버 출력 모델 구현 - BOJ4673.py 생성

---
+++ BOJ4673.py
@@ -0,0 +1,13 @@
+# 백준 4673번 : 셀프 넘버 (실버 V)
+
+num_set = set(range(1, 10000))
+remove_set = set() # 생성자가 있는 숫자를 추가
+
+for num in num_set:
+    for i in str(num): # 각 자릿수 별로 더하기 위해 문자로 변환
+        num += int(i)
+    remove_set.add(num)
+
+self_numbers = num_set - remove_set
+for self_num in sorted(self_numbers): # 오름차순으로 출력
+    print(self_num)
