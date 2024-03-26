1cf8b0ac6dce7585a36b4708027ac53adbfb5a2c
Sonhyeong-oh <daily6003@gmail.com>
2024-03-26 16:56:34 +0900

    feat: 우선순위 큐 정렬기준 - BOJ11286.py

---
+++ BOJ11286.py
@@ -0,0 +1,34 @@
+# 백준 11286번 : 절댓값 힙 (실버 I) original model
+
+from queue import PriorityQueue
+# 우선순위 큐 사용을 위한 파이썬 내장함수 PriorityQueue 호출 
+# 리스트 시간복잡도 : input = O(1), pop = O(N)
+# 힙(우선순위 큐) = input, pop = O(logN)
+from sys import stdin, stdout
+import math
+
+input = stdin.readline
+print = stdout.write
+
+N = int(input().rstrip())
+que = PriorityQueue()
+# 최대크기 제한 = PriorityQueue(maxsize=number)
+
+for _ in range(N):
+    word = int(input().rstrip())
+    if word == 0:
+        if que.empty() == True:
+            # 힙이 비어있다면
+            print('0\n')
+        else:
+            print("{0}\n".format(que.get()[1]))
+            # get = pop, pop과 마찬가지로 get한 value를 리턴
+            # 우선순위 큐는 자료를 put 순서와 상관없이 오름차순 정렬, get도 작은 숫자 순서대로 반환
+    else:
+        que.put((abs(word), word))
+        # put = append
+        # priority queue의 value가 튜플 형태라면 LHS 비교 후 RHS 비교, 오름차순 정렬
+
+# len(que) = que.qsize()
+# que.empty() -> print by boolean
+# que.full() -> que가 maxsize와 일치한다면 True 아니면 False
